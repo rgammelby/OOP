7ed2f5ea13d05674abbd29e1808cb98a1ee727f2
rgammelby <rab.gammelby@gmail.com>
2023-09-11 10:38:45 +0200

    Superheltene uploaded

---
+++ Superheltene/Superheltene/Superheltene/Costume.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheltene
+{
+    internal class Costume
+    {
+        private bool _cape;
+        private bool _mask;
+        private string _colour1;
+        private string _colour2;
+        private string _colour3;
+
+        public bool Cape
+        {
+            get { return _cape; }
+            private set { _cape = value; }
+        }
+
+        public bool Mask
+        { 
+            get { return _mask; }
+            private set {  _mask = value; } 
+        }
+
+        public string Colour1 
+        { 
+            get { return _colour1; } 
+            private set {  _colour1 = value; } 
+        }
+
+        public string Colour2 
+        { 
+            get { return _colour2; } 
+            private set { _colour2 = value; } 
+        }
+
+        public string Colour3 
+        { 
+            get { return _colour3; } 
+            private set { _colour3 = value; } 
+        }
+    }
+}
---
+++ Superheltene/Superheltene/Superheltene/Superhero.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superheltene
+{
+    /// <summary>
+    /// Class <c>Superhero</c> models a superhero or villain.
+    /// </summary>
+
+    internal class Superhero
+    {
+        private string _ability = null;
+        private string _equipment = null;
+        private string _alias;
+        private Superhero _archenemy;
+        private bool _morality;
+        private string _name;
+        private string _issue = null;
+        // TODO: Add costume.
+
+        private const bool GOOD = true;
+        private const bool EVIL = false;
+
+        public string Ability 
+        { 
+            get { return _ability; } 
+            private set { _ability = value; } 
+        }
+
+        public string Equipment 
+        { 
+            get { return _equipment; } 
+            private set { _equipment = value; } 
+        } 
+
+        public string Alias 
+        { 
+            get { return _alias; } 
+            private set {  _alias = value; } 
+        }
+
+        public Superhero Archenemy
+        {
+            get { return _archenemy; }
+            private set { _archenemy = value; }
+        }
+
+        public bool Morality 
+        { 
+            get { return _morality; } 
+            private set { _morality = value; } 
+        }
+
+        public string Name 
+        { 
+            get { return _name; } 
+            private set { _name = value; } 
+        }
+
+        public string Issue
+        {
+            get { return _issue; }
+            private set { _issue = value; }
+        }
+    }
+}
