69ae3865c852a214c28bbb9597b6ac5da3d8a695
rgammelby <rab.gammelby@gmail.com>
2023-09-13 13:38:45 +0200

    blackjack 2

---
+++ Blackjack2 EQUALLY UNFINISHED/Blackjack2/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack2
+{
+    internal class Model
+    {
+        enum Broadway { J = 11, Q, K }
+    }
+}
