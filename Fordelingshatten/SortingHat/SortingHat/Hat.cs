b97fa13c01f409f69af96a55543a05da0d3a3b22
rgammelby <rab.gammelby@gmail.com>
2023-09-13 09:32:51 +0200

    sorting hat assignment added

---
+++ Fordelingshatten/SortingHat/SortingHat/Hat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingHat
+{
+    enum Houses { Gryffindor, Ravenclaw, Hufflepuff, Slytherin }
+
+    public class Hat
+    {
+        private string _house;
+
+        public string House
+        {
+            get { return _house; }
+            private set { _house = value; }
+        }
+
+        public Random r = new Random();
+        public string SetHouse()
+        {
+            var house = r.Next(0, 4);
+            return ((Houses)house).ToString();
+        }
+    }
+}
---
+++ Fordelingshatten/SortingHat/SortingHat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingHat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Hat h = new Hat();
+                var house = h.SetHouse();
+
+                Console.WriteLine(house);
+                Console.ReadLine();
+            }            
+        }
+    }
+}
