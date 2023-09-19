f64450ce9c0d1c9d712c2a9ed103302647eef26e
rgammelby <rab.gammelby@gmail.com>
2023-09-19 13:28:40 +0200

    the firm 2 work in progress

---
+++ The Firm 2/TheFirm2/TheFirm2/CFO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFirm2
+{
+    internal class CFO
+    {
+        public void AssignFunds()
+        {
+
+        }
+    }
+}
